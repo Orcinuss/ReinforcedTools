7a8b00f89917237645726ecb6b42b738221c8516
Orcinuss <odelphinidae@gmail.com>
2014-07-07 23:07:44 +0200

    Attempt at adding Harvester enchant

--- src/main/java/com/orcinuss/reinforcedtools/item/tools/ItemRTMHarvester.java
+++ src/main/java/com/orcinuss/reinforcedtools/item/tools/ItemRTMHarvester.java
@@ -4,9 +4,11 @@ import com.google.common.collect.Sets;
 import com.orcinuss.reinforcedtools.creativetab.CreativeTab;
 import com.orcinuss.reinforcedtools.init.ModItems;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 
 import java.util.Set;
@@ -26,4 +28,6 @@ public class ItemRTMHarvester extends ItemTool{
         this.maxStackSize = 1;
         this.setCreativeTab(CreativeTab.RTM_TAB);
     }
+
+    //ItemStack BlockHarvesterStack = new ItemStack(ModItems.toolBlockHarvester, BlockHarvesterStack.addEnchantment(Enchantment.silkTouch, 1));
 }
